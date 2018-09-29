b8e1b0036f46e3e3877a8f5f04ef24fe6427d151
AngelesOct <171711o255@utectulancingo.edu.mx>
2018-09-28 21:44:00 -0500

    modificaiones para tener la version 2 terminada del CSV

--- src/controllers/ControllerCsv.java
+++ src/controllers/ControllerCsv.java
@@ -6,10 +6,14 @@
 package controllers;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import models.ModelCsv;
 import views.ViewCsv;
@@ -21,23 +25,45 @@ import views.ViewCsv;
 public class ControllerCsv {
     ModelCsv  modelCsv = new ModelCsv();
     ViewCsv viewCsv = new ViewCsv();
-
+    /**
+     * Va verificando que boton es el que se ha seleccionado y llama a su funcion que le corresponde
+     */
     ActionListener al = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            if(e.getSource()== viewCsv.jb_nuevo) jb_nuevo_action_performed();
-            if(e.getSource()== viewCsv.jb_guardar) jb_guardar_action_performed();
+            if(e.getSource()== viewCsv.jb_nuevo) jb_nuevo_action_performed(); //boton NUEVO
+            if(e.getSource()== viewCsv.jb_guardar) jb_guardar_action_performed(); //boton GUARDAR
+            if(e.getSource()== viewCsv.jb_primero) jb_primero_action_performed(); //boton PRIMERO
+            if(e.getSource()== viewCsv.jb_siguiente) jb_siguiente_action_performed(); //boton SIGUIENTE
+            if(e.getSource()== viewCsv.jb_anterior) jb_anterior_action_performed(); //boton ANTERIOR
+            if(e.getSource()== viewCsv.jb_ultimo) jb_ultimo_action_performed(); //boton ULTIMO
         }
     };
-
+    /**
+     * integra los componentes del modelo MVC
+     * @param viewCsv
+     * @param modelCsv 
+     */
     public ControllerCsv(ViewCsv viewCsv, ModelCsv modelCsv) {
         viewCsv.setVisible(true);
         this.modelCsv = modelCsv;
         this.viewCsv = viewCsv;
         this.viewCsv.jb_nuevo.addActionListener(al);
         this.viewCsv.jb_guardar.addActionListener(al);
+        this.viewCsv.jb_primero.addActionListener(al);
+        this.viewCsv.jb_siguiente.addActionListener(al);
+        this.viewCsv.jb_anterior.addActionListener(al);
+        this.viewCsv.jb_ultimo.addActionListener(al);
+        this.primero(modelCsv.getPath()); //mostrara el primer registro al iniiar la aplicacion 
     }
     
+    /**
+     *-----Metodos para cada boton, y este tenga su respectiva funcion-----
+     */
+    
+    /**
+    * En este metodo se llama al metodo de limpiar
+    */  
     public void jb_nuevo_action_performed(){
         this.Limpiar();
     }
@@ -47,6 +73,35 @@ public class ControllerCsv {
     */
     public void jb_guardar_action_performed(){
        this.writeFile(modelCsv.getPath());
+       this.ultimo(modelCsv.getPath()); //llama al ultimo registro guardado
+    }
+    /**
+    * En este metodo se llama al metodo de primero
+    * y se le agrega su parametro para que pueda realizar su funcion de manera correcta
+    */ 
+    public void jb_primero_action_performed(){
+        this.primero(modelCsv.getPath());
+    }
+    /**
+    * En este metodo se llama al metodo de siguiente
+    * y se le agrega su parametro para que pueda realizar su funcion de manera correcta
+    */ 
+    public void jb_siguiente_action_performed(){
+        this.siguiente(modelCsv.getPath());
+    }
+    /**
+    * En este metodo se llama al metodo de anterior
+    * y se le agrega su parametro para que pueda realizar su funcion de manera correcta
+    */ 
+    public void jb_anterior_action_performed(){
+        this.anterior(modelCsv.getPath());
+    }
+    /**
+    * En este metodo se llama al metodo de ultimo
+    * y se le agrega su parametro para que pueda realizar su funcion de manera correcta
+    */ 
+    public void jb_ultimo_action_performed(){
+        this.ultimo(modelCsv.getPath());
     }
     /**
     * Escribe nuevas lineas en el archivo
@@ -55,13 +110,13 @@ public class ControllerCsv {
         try{
             File  file = new File(path); //Ruta del arhivo que se abrira
             FileWriter fileWriter = new FileWriter(file,true);
-            modelCsv.setNombre(viewCsv.jtf_nombre.getText());
+            modelCsv.setNombre(viewCsv.jtf_nombre.getText()); // se agrega el contenido de la caja de texto a las variables
             modelCsv.setEmail(viewCsv.jtf_email.getText());
-            modelCsv.setGuardar(modelCsv.getNombre() + "," + modelCsv.getEmail());
+            modelCsv.setGuardar(modelCsv.getNombre() + ";" + modelCsv.getEmail());
             try(PrintWriter printWriter = new PrintWriter(fileWriter)){               
-                printWriter.println(modelCsv.getGuardar());
+                printWriter.println(modelCsv.getGuardar()); //se guarda cada campo agregado
                 printWriter.close();  
-                JOptionPane.showMessageDialog(null,"Guardado con Exito");
+                JOptionPane.showMessageDialog(null,"Guardado con Exito"); //notificacion de que los datos fueron guardados
         }catch(Exception e){
             JOptionPane.showMessageDialog(null,"File not found:"+e.getMessage());
         }
@@ -69,10 +124,128 @@ public class ControllerCsv {
             JOptionPane.showMessageDialog(null,"error on IO operation:"+err.getMessage());
         } 
        return null;   
-    }//WriteFile   
+    }//WriteFile  
+    /**
+     * este metodo va a limpiar cada caja de texto, para poder ingresar nuevos datos
+     * @return 
+     */
     public String Limpiar(){
         viewCsv.jtf_nombre.setText(modelCsv.getLimpiar_cajas());
         viewCsv.jtf_email.setText(modelCsv.getLimpiar_cajas());
         return null;
+    }
+    
+    
+//----------Metodos para los botones de movimientos agregados a la version 2 de la aplicacion--------
+    /**
+     * va ha mostrar en cada caja de texto el primer dato dentro del archivo
+     * @param path
+     * @return 
+     */
+    public String primero(String path){
+        try{  
+            String acumula_lineas =""; //acomulara cada linea para 
+            String row; // indica una fila
+            try (FileReader archivo = new FileReader(path)){              
+                BufferedReader bufferedReader = new BufferedReader(archivo);
+                    while ((row=bufferedReader.readLine())!= null){
+                       acumula_lineas+=row+"¬"; // si el archivo tiene mas filas con esta linea ira mostrando cada una de ellas
+                    }
+                    String datos[] =  acumula_lineas.split("¬");//la sepacion de cada linea sera el simbolo (¬)
+                    modelCsv.setPosicion(0);
+                    String registro[] = datos[modelCsv.getPosicion()].split(";");
+                    viewCsv.jtf_nombre.setText(registro[0]);
+                    viewCsv.jtf_email.setText(registro[1]); // posicion de los registros que seran mostrados              
+            } catch(FileNotFoundException err){
+                JOptionPane.showMessageDialog(null,"File not found"+err.getMessage());
+            }        
+        } catch(IOException err){
+            JOptionPane.showMessageDialog(null,"error on IO operation:"+err.getMessage());
+        } 
+        return null;    
+    }
+    public String siguiente(String path){
+        try{  
+            String acumula_lineas =""; //acomulara cada linea para 
+            String row; // indica una fila
+            try (FileReader archivo = new FileReader(path)){              
+                BufferedReader bufferedReader = new BufferedReader(archivo);
+                int contador =0; //contador de cada fila
+                    while ((row=bufferedReader.readLine())!= null){
+                       contador = contador + 1;
+                       acumula_lineas+=row+"¬"; // si el archivo tiene mas filas con esta linea ira mostrando cada una de ellas
+                    }
+                    String datos[] =  acumula_lineas.split("¬");//la sepacion de cada linea sera el simbolo (¬)  
+                    modelCsv.setPosicion(modelCsv.getPosicion()+1); //va al siguiente valor
+                    if (modelCsv.getPosicion() < contador){
+                        String registro[] = datos[modelCsv.getPosicion()].split(";"); // ira a los siguientes datos antes del ultimo
+                        viewCsv.jtf_nombre.setText(registro[0]);
+                        viewCsv.jtf_email.setText(registro[1]); // posicion de los registros que seran mostrados 
+                    }  
+                    else{
+                        modelCsv.setPosicion(modelCsv.getPosicion()-1); 
+                        JOptionPane.showMessageDialog(null,"ha llegado al ultimo dato");
+                    }
+            } catch(FileNotFoundException err){
+                JOptionPane.showMessageDialog(null,"File not found"+err.getMessage());
+            }        
+        } catch(IOException err){
+            JOptionPane.showMessageDialog(null,"error on IO operation:"+err.getMessage());
+        } 
+        return null;    
+    }
+    public String anterior(String path){
+        try{  
+            String acumula_lineas =""; //acomulara cada linea para 
+            String row; // indica una fila
+            try (FileReader archivo = new FileReader(path)){              
+                BufferedReader bufferedReader = new BufferedReader(archivo);
+                int contador =0; //contador de cada fila
+                    while ((row=bufferedReader.readLine())!= null){
+                       contador = contador + 1;
+                       acumula_lineas+=row+"¬"; // si el archivo tiene mas filas con esta linea ira mostrando cada una de ellas
+                    }
+                    String datos[] =  acumula_lineas.split("¬");//la sepacion de cada linea sera el simbolo (¬)  
+                    modelCsv.setPosicion(modelCsv.getPosicion()-1); //va al siguiente valor
+                    if (modelCsv.getPosicion()>=0){
+                        String registro[] = datos[modelCsv.getPosicion()].split(";"); // ira a los siguientes datos antes del ultimo
+                        viewCsv.jtf_nombre.setText(registro[0]);
+                        viewCsv.jtf_email.setText(registro[1]); // posicion de los registros que seran mostrados 
+                    }  
+                    else{
+                        modelCsv.setPosicion(modelCsv.getPosicion()+1); 
+                        JOptionPane.showMessageDialog(null,"ha llegado al primer dato");
+                    }
+            } catch(FileNotFoundException err){
+                JOptionPane.showMessageDialog(null,"File not found"+err.getMessage());
+            }        
+        } catch(IOException err){
+            JOptionPane.showMessageDialog(null,"error on IO operation:"+err.getMessage());
+        }        
+        return null;    
+    }
+    public String ultimo(String path){
+        try{  
+            String acumula_lineas =""; //acomulara cada linea para 
+            String row; // indica una fila
+            try (FileReader archivo = new FileReader(path)){              
+                BufferedReader bufferedReader = new BufferedReader(archivo);
+                int contador=0;
+                    while ((row=bufferedReader.readLine())!= null){
+                        contador = contador + 1;
+                       acumula_lineas+=row+"¬"; // si el archivo tiene mas filas con esta linea ira mostrando cada una de ellas
+                    }
+                    String datos[] =  acumula_lineas.split("¬");//la sepacion de cada linea sera el simbolo (¬)
+                    modelCsv.setPosicion(contador-1);
+                    String registro[] = datos[modelCsv.getPosicion()].split(";");
+                    viewCsv.jtf_nombre.setText(registro[0]);
+                    viewCsv.jtf_email.setText(registro[1]); // posicion de los registros que seran mostrados              
+            } catch(FileNotFoundException err){
+                JOptionPane.showMessageDialog(null,"File not found"+err.getMessage());
+            }        
+        } catch(IOException err){
+            JOptionPane.showMessageDialog(null,"error on IO operation:"+err.getMessage());
+        } 
+        return null;    
     }
 }
